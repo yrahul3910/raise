2d9ecbb03373834a6ac6d441874ec45c5726a198
Rahul Yedida <yrahul3910@gmail.com>
2020-09-30 00:23:40 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-import distutils.core
+from setuptools import setup
 
-distutils.core.setup(name='raise_utils',
-                     version='1.2',
-                     description='RAISE lab package (LGPL-3.0-or-later)',
-                     author='RAISE, NC State University',
-                     author_email='ryedida@ncsu.edu',
-                     packages=[
-                         'raise_utils.data',
-                         'raise_utils.experiments',
-                         'raise_utils.hyperparams',
-                         'raise_utils.learners',
-                         'raise_utils.metrics',
-                         'raise_utils.transform',
-                         'raise_utils.transform.text',
-                         'raise_utils.interpret',
-                         'raise_utils.hooks',
-                         'raise_utils'
-                     ]
-                     )
+setup(name='raise_utils',
+      version='1.2',
+      description='RAISE lab package (LGPL-3.0-or-later)',
+      author='RAISE, NC State University',
+      author_email='ryedida@ncsu.edu',
+      packages=[
+          'raise_utils.data',
+          'raise_utils.experiments',
+          'raise_utils.hyperparams',
+          'raise_utils.learners',
+          'raise_utils.metrics',
+          'raise_utils.transform',
+          'raise_utils.transform.text',
+          'raise_utils.interpret',
+          'raise_utils.hooks',
+          'raise_utils'
+      ]
+      )
