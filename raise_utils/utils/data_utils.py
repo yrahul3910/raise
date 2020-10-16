9636f7bc3bc341b3e94e78ee8f3119beee52ac6e
Rahul Yedida <yrahul3910@gmail.com>
2020-10-15 23:56:54 -0400

    Fixed tests

--- raise_utils/utils/data_utils.py
+++ raise_utils/utils/data_utils.py
@@ -2,30 +2,30 @@ from raise_utils.data import Data
 
 
 def _check_data(data: Data) -> None:
-      """
-        Ensures data is set
+    """
+    Ensures data is set
 
-        :return: None
-        """
+    :return: None
+    """
 
-       if (
-            data.x_train is None or
-            data.y_train is None or
-            data.x_test is None or
-            data.y_test is None
+    if (
+        data.x_train is None or
+        data.y_train is None or
+        data.x_test is None or
+        data.y_test is None
+    ):
+        raise AssertionError("Train/test data is None.")
+
+    if len(data.x_train.shape) == 2:
+        if (
+            data.x_train.shape[0] != data.y_train.shape[0] or
+            data.x_test.shape[0] != data.y_test.shape[0] or
+            data.x_train.shape[1] != data.x_test.shape[1]
         ):
-             raise AssertionError("Train/test data is None.")
-
-        if len(data.x_train.shape) == 2:
-            if (
-                data.x_train.shape[0] != data.y_train.shape[0] or
-                data.x_test.shape[0] != data.y_test.shape[0] or
-                data.x_train.shape[1] != data.x_test.shape[1]
-            ):
-                raise AssertionError("Train/test data have a shape mismatch.")
-        else:
-            if (
-                data.x_train.shape[0] != data.y_train.shape[0] or
-                data.x_test.shape[0] != data.y_test.shape[0]
-            ):
-                raise AssertionError("x/y shape mismatch.")
+            raise AssertionError("Train/test data have a shape mismatch.")
+    else:
+        if (
+            data.x_train.shape[0] != data.y_train.shape[0] or
+            data.x_test.shape[0] != data.y_test.shape[0]
+        ):
+            raise AssertionError("x/y shape mismatch.")
