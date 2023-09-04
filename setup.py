3fd0075b096ade3370ba5690c9718585877e9957
Rahul Yedida <rahul.yedida@pm.me>
2023-09-03 20:31:09 -0400

    Fix versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 ext_modules = cythonize("raise_utils/transforms/remove_labels.pyx")
 
 setup(name='raise_utils',
-      version='2.0.3',
+      version='2.0.4',
       description='RAISE lab package (LGPL-3.0-or-later)',
       author='RAISE, NC State University',
       author_email='ryedida@ncsu.edu',
@@ -35,7 +35,9 @@ setup(name='raise_utils',
           'colorama',
           'hyperopt',
           'ray[tune]',
-          'imblearn'
+          'imblearn',
+          'Cython',
+          'tabulate'
       ],
       ext_modules=ext_modules
       )
