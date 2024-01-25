c5028910c411c6125b68da7be90408ea972fb1c5
Rahul Yedida <rahul.yedida@pm.me>
2024-01-25 01:08:09 -0500

    Bug fix

--- sphinx-docs/conf.py
+++ sphinx-docs/conf.py
@@ -9,7 +9,7 @@
 project = 'raise-utils'
 copyright = '2024, Rahul Yedida'
 author = 'Rahul Yedida'
-release = '2.1.3'
+release = '2.1.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
