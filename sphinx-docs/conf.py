236dba9ae1ce0bbe4489b5275891172066a8df1f
Rahul Yedida <yrahul3910@gmail.com>
2024-11-17 17:37:36 -0500

    Update docs

--- sphinx-docs/conf.py
+++ sphinx-docs/conf.py
@@ -9,12 +9,23 @@
 project = 'raise-utils'
 copyright = '2024, Rahul Yedida'
 author = 'Rahul Yedida'
-release = '2.1.4'
+release = '2.5.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc']
+
+autodoc_mock_imports = [
+    'numpy', 
+    'pandas', 
+    'sklearn', 
+    'keras',
+    'imblearn',
+    'colorama'
+    'scipy',
+    'cvxopt'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
